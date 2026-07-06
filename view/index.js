bfc55ce8c4dc63bac7e495947d88e4a690ac9972
Ajaxparmar <ajaxparmar@gmail.com>
2026-07-06 11:22:44 +0530

    add lested code

---
+++ view/index.js
@@ -0,0 +1,40 @@
+
+// // {
+// // let username="vijay"
+// // username="ravi"
+// // console.log("Hello", username);
+// // }
+// // const a=[];
+// // console.log(a);
+
+// let a=90;
+// let username='vijay kumar'
+// let rollno=123
+// let Class ='mac'
+
+// let mystring='this is ""'
+
+// console.log(`Hello ${username} this is you class ${Class} and roll no is ${rollno}`);
+
+
+
+
+
+
+// const mylist=[3,4,5]
+// mylist[2]=90
+// console.log(mylist);
+
+
+let a=2
+let b=2
+let x = true;
+let y = x ||= 10;
+
+let e
+
+
+console.log(y);
+
+
+    
